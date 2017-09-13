c697bd08bdafdaba4139a3a97ddc17749cc41599
Pavlo Timofieiev <pavliashvili@ukr.net>
2017-09-13 23:04:02 +0300

    com

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import view from './view'
 import model from './model'
+
+
 // Display initial state
 view.display(view.init(model));
 // view.display(view.init(model)) ;
